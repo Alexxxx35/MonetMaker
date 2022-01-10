35f100d5c945be31c92d36b80d84963206cb9fd1
boury_a <boury_a@etna-alternance.net>
2022-01-10 11:00:18 +0100

    fix

--- Step_02/main.py
+++ Step_02/main.py
@@ -3,12 +3,14 @@ import cv2 as cv
 import argparse
 from enum import Enum
 
+
 class blur(Enum):
-    linear= 'linear'
+    linear = 'linear'
     gaussian = 'gaussian'
     radial = 'radial'
     median = 'median'
     bilateral = 'bilateral'
+
     def str(self):
         return self.value
 
@@ -16,12 +18,10 @@ class blur(Enum):
 cli = argparse.ArgumentParser()
 cli.add_argument("-i", "--image", required=True,
                  help="image path")
-
-cli.add_argument("-b","--blur", type=blur, choices=list(blur), required=False,
-    help="Blur method pre-applied before Canny Edge Detection (linear, gaussian or radial)")
-
-cli.add_argument("-k","--kernel", required=False,
-    help="Blur method pre-applied before Canny Edge Detection (linear, gaussian or radial)")
+cli.add_argument("-b", "--blur", type=blur, choices=list(blur), required=False,
+                 help="Blur method pre-applied before Canny Edge Detection (linear, gaussian or radial)")
+cli.add_argument("-k", "--kernel", required=False,
+                 help="kernel level of blurring")
 
 args = vars(cli.parse_args())
 
@@ -30,31 +30,29 @@ img = cv.imread(args["image"])
 # noise reduction
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-#Apply Blur method
+# Apply Blur method
 if "blur" in args:
     if args["blur"] == "linear":
-        blurred = cv.boxFilter(gray,-1,int(args["kernel"]),normalize=False,) if args["kernel"] else cv.boxFilter(gray,-1,3,normalize=False)
-
-    elif args["blur"] == "gaussian":
-        blurred = cv.GaussianBlur(gray,int(args["kernel"]),0) if args["kernel"] else cv.GaussianBlur(gray,3,0)
-
+        blurred = cv.boxFilter(gray, -1, int(args["kernel"]), normalize=False,
+                               ) if args["kernel"] else cv.boxFilter(gray, -1, 3, normalize=False)
     elif args["blur"] == "median":
-        blurred = cv.medianBlur(gray,int(args["kernel"])) if args["kernel"] else cv.medianBlur(gray,3)
+        blurred = cv.medianBlur(
+            gray, int(args["kernel"])) if args["kernel"] else cv.medianBlur(gray, 3)
     elif args["blur"] == "bilateral":
-        blurred = cv.bilateralFilter(gray,int(args["kernel"]),75,75) if args["kernel"] else cv.medianBlur(gray,9,75,75)
+        blurred = cv.bilateralFilter(gray, int(
+            args["kernel"]), 75, 75) if args["kernel"] else cv.medianBlur(gray, 9, 75, 75)
     else:
-        a=1
-
-
-
-
+        # gaussian blur by default
+        blurred = cv.GaussianBlur(gray, (int(
+            args["kernel"]), int(
+            args["kernel"])), 0) if args["kernel"] else cv.GaussianBlur(gray, (3, 3), 0)
 
 
 computed_median = np.median(gray)
 # apply automatic Canny edge detection using the computed median
 lower_treshold = (1.0 - 0.33) * computed_median
 
-#Here we consider that the max value the image is 255 ( 8bit images)
+# Here we consider that the max value the image is 255 ( 8bit images)
 upper_treshold = int(min(255, (1.0 + 0.33) * computed_median))
 
 
@@ -67,12 +65,9 @@ wide = ~cv.Canny(img, 10, 200)
 tight = ~cv.Canny(img, 225, 250)
 
 while True:
-    cv.imshow("Canny Edge Detection", np.hstack([tight,wide,automatically_edged]))
+    cv.imshow("Canny Edge Detection", np.hstack(
+        [tight, wide, automatically_edged]))
 
-    # black & white inversion
-    #cv.imshow("Lion_edge", ~automatically_edged)
     if cv.waitKey(1) == 27:
         cv.destroyAllWindows()
         break
-
-
--- Step_03/main.py
+++ Step_03/main.py
@@ -1,17 +1,32 @@
+import turtle
 import numpy as np
 import cv2 as cv
 import argparse
-import turtle
+from enum import Enum
 
 WIDTH, HEIGHT = 800, 600
 window_size = []
-pixel_counter=0
+
+
+class blur(Enum):
+    linear = 'linear'
+    gaussian = 'gaussian'
+    radial = 'radial'
+    median = 'median'
+    bilateral = 'bilateral'
+
+    def str(self):
+        return self.value
+
 
 cli = argparse.ArgumentParser()
 cli.add_argument("-i", "--image", required=True,
                  help="image path")
-cli.add_argument("-b", "--blur", required=False,
-                 help="gaussian blur intensity")
+
+cli.add_argument("-b", "--blur", type=blur, choices=list(blur), required=False,
+                 help="blur method pre-applied before Canny Edge Detection (linear, gaussian or radial)")
+cli.add_argument("-k", "--kernel", required=False,
+                 help="kernel level of blurring")
 cli.add_argument("-W", "--width", required=False,
                  help="window width")
 cli.add_argument("-H", "--height", required=False,
@@ -22,10 +37,32 @@ cli.add_argument("-s", "--speed", required=False,
 args = vars(cli.parse_args())
 
 img = cv.imread(args["image"])
+
+# noise reduction
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+
+# Apply Blur method
+if "blur" in args:
+    if args["blur"] == "linear":
+        blurred = cv.boxFilter(gray, -1, int(args["kernel"]), normalize=False,
+                               ) if args["kernel"] else cv.boxFilter(gray, -1, 3, normalize=False)
+    elif args["blur"] == "median":
+        blurred = cv.medianBlur(
+            gray, int(args["kernel"])) if args["kernel"] else cv.medianBlur(gray, 3)
+    elif args["blur"] == "bilateral":
+        blurred = cv.bilateralFilter(gray, int(
+            args["kernel"]), 75, 75) if args["kernel"] else cv.medianBlur(gray, 9, 75, 75)
+    else:
+        # gaussian blur by default
+        blurred = cv.GaussianBlur(gray, (int(
+            args["kernel"]), int(
+            args["kernel"])), 0) if args["kernel"] else cv.GaussianBlur(gray, (3, 3), 0)
+
 if "speed" in args and args["speed"]:
     drawing_speed = int(args["speed"])
 else:
-    drawing_speed = 1
+    drawing_speed = 0
 
 if "width" in args and "height" in args and args["width"] and args["height"]:
     window_size = (int(args["width"]), int(args["height"]))
@@ -33,43 +70,32 @@ else:
     window_size.append(WIDTH)
     window_size.append(HEIGHT)
 
-
-# noise reduction
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-if "blur" in args and args["blur"]:
-    if int(args["blur"]) % 2 == 0:
-        raise AssertionError(
-            "gaussian blur intensity must be defined by an odd number")
-    else:
-        blur_intensity = (int(args["blur"]), int(args["blur"]))
-        img = cv.GaussianBlur(gray, blur_intensity, 1)
-else:
-    img = gray
-
-# compute the median of the single channel pixel intensities
-computed_median = np.median(img)
-
+computed_median = np.median(gray)
 # apply automatic Canny edge detection using the computed median
-lower = int(max(0, (1.0 - 0.33) * computed_median))
-upper = int(min(255, (1.0 + 0.33) * computed_median))
-automatically_edged = cv.Canny(img, lower, upper)
+lower_treshold = (1.0 - 0.33) * computed_median
+
+# Here we consider that the max value the image is 255 ( 8bit images)
+upper_treshold = int(min(255, (1.0 + 0.33) * computed_median))
+
+
+automatically_edged = ~cv.Canny(img, lower_treshold, upper_treshold)
+
 
 # apply Canny edge detection using a wide threshold, tight
 # threshold, and automatically determined threshold
-wide = cv.Canny(img, 10, 200)
-tight = cv.Canny(img, 225, 250)
+wide = ~cv.Canny(img, 10, 200)
+tight = ~cv.Canny(img, 225, 250)
 
 # while True:
-#     #cv.imshow("Lion_edge", np.hstack([wide, tight, automatically_edged]))
+#     cv.imshow("Canny Edge Detection", np.hstack(
+#         [tight, wide, automatically_edged]))
 
-#     # black & white inversion
-#     cv.imshow("Lion_edge", ~automatically_edged)
 #     if cv.waitKey(1) == 27:
 #         cv.destroyAllWindows()
 #         break
 
-retval, dst = cv.threshold(~automatically_edged, 127, 255, cv.THRESH_BINARY)
+
+retval, dst = cv.threshold(automatically_edged, 127, 255, cv.THRESH_BINARY)
 width = int(img.shape[1])
 height = int(img.shape[0])
 
@@ -80,10 +106,9 @@ screen.screensize(window_size[0], window_size[1])
 
 pen = turtle.Turtle()
 pen.hideturtle()
-screen.tracer(0)
+screen.tracer(drawing_speed)
 
 for y in range(int(height/2), int(height/-2),  -1):
-    print(pixel_counter)
     pen.penup()
     pen.goto(-(width / 2), y)
 
@@ -96,9 +121,6 @@ for y in range(int(height/2), int(height/-2),  -1):
         else:
             pen.penup()
             pen.forward(1)
-    pixel_counter+=1
-    if pixel_counter % drawing_speed == 0: 
-        screen.update()
 
 
 turtle.done()
